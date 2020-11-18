a2036a41a8992fb14c5e1aa1e0ad948391f9c2e0
codingriver <codingriver@163.com>
2020-11-18 17:49:34 +0800

    QuickSorting,InsertSort

---
+++ InsertSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Codingriver
+{
+    /// <summary>
+    /// 插入排序(E版本)(优化版本)(稳定排序)
+    /// 时间复杂度:
+    ///     最优情况双层循环次数：内循环次数 i=1(1),i=2(1),...,i=n-2(1)，总次数=(n-2) 
+    ///     最优情况每次比较都不需要交换，执行内循环一次时复杂度为1（比较一次），所以复杂度=2*(n-2),O=2(n-2)=O(n)
+    /// </summary>
+    public class InsertSort
+    {
+        /// <summary>
+        /// 插入排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        public static void Sort<T>(T[] data) where T : IComparable
+        {
+            int len = data.Length;
+            Sort(data, 0, len - 1);
+        }
+        /// <summary>
+        /// 插入排序
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="low">数组下标最小值</param>
+        /// <param name="high">数组下标最大值</param>
+        public static void Sort<T>(T[] data, int low, int high) where T : IComparable
+        {
+            T tmp;
+            int j;
+            for (int i = low+1; i <= high; i++) //第一个当做有序序列
+            {
+                tmp = data[i];
+                for (j = i; j > 0 && data[j - 1].CompareTo(tmp) > 0; j--) //内循环使用冒泡方式对前面有序序列进行插入
+                {
+                    data[j] = data[j - 1];
+                }
+                data[j] = tmp;
+            }
+
+
+        }
+
+    }
+}
---
+++ QuickSorting.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codingriver
+{
+    /// <summary>
+    /// 快速排序
+    /// 简单说是给基准数找正确索引位置的过程.
+    /// 快速排序是对冒泡排序的一种改进。
+    /// 首先选取一个初始值（一般选取待排序序列的第一个值），通过一趟排序将待排序序列分成两个子序列，使左子序列的所有数据都小于这个初始值，右子序列的所有数据都大于这个初始值，然后再按此方法分别对这两个子序列进行排序，递归的进行上面的步骤，直至每一个数据项都有如下性质：该数据项左边的数据都小于它，右边的数据都大于它，这样，整个序列就有序了。
+    /// 时间复杂度：O=O(nlogn)和归并排序推理类似，不再展开推理了
+    /// 当数据长度小于等于20或者分割后小于等于20时自动使用插入排序
+    /// </summary>
+    public class QuickSorting
+    {
+
+        /// <summary>
+        /// 快速排序（递归版）（不稳定排序）(优先使用)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        public static void Sort<T>(T[] data) where T : IComparable
+        {
+            int len = data.Length;
+            Sort(data, 0, len - 1);
+        }
+        private static void Sort<T>(T[] data, int low, int high) where T : IComparable
+        {
+            if (low >= high) return;
+            if(high-low+1<=20)
+            {
+                InsertSort.Sort(data,low,high);
+                return;
+            }
+
+            int split = Partition(data, low, high);
+            Sort(data, low, split - 1);
+            Sort(data, split + 1, high);
+        }
+
+        private static int Partition<T>(T[] data,int low,int high) where T : IComparable
+        {
+            T pivot = data[low];
+            while (low<high)
+            {
+                while (low < high && data[high].CompareTo(pivot) >= 0) --high;
+                data[low] = data[high];
+                while (low < high && data[low].CompareTo(pivot) <= 0) ++low;
+                data[high] = data[low];
+            }
+            data[low] = pivot;
+            return low;
+        }
+
+
+        /// <summary>
+        /// 快速排序（非递归版）（不稳定排序）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        public static void Sort_V<T>(T[] data) where T : IComparable
+        {
+            Stack<int> stack = new Stack<int>(data.Length/2);
+            T pivot;
+            int low = 0;
+            int high = data.Length - 1;
+            int start, end;
+            stack.Push(high);
+            stack.Push(low);
+
+            while (stack.Count > 0)
+            {
+                start = low = stack.Pop();
+                end = high = stack.Pop();
+
+                if (low >= high)
+                    continue;
+                if (high - low + 1 <= 20)
+                {
+                    InsertSort.Sort(data, low, high);
+                    continue;
+                }
+
+                pivot = data[low];
+                while (low < high)
+                {
+                    while (low < high && data[high].CompareTo( pivot)>=0) high--;
+                    data[low] = data[high];
+
+                    while (low < high && data[low].CompareTo( pivot)<=0) low++;
+                    data[high] = data[low];
+                }
+                data[low] = pivot;
+                stack.Push(low - 1);
+                stack.Push(start);
+                stack.Push(end);
+                stack.Push(low + 1);
+
+            }
+        }
+
+    }
+
+}
+
+
